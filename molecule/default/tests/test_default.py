d0c542d4968264707b7f8f4e6bf8afb371b81040
@slovacus <slovacus@gmail.com>
2019-05-04 11:15:02 -0500

    (#9) test: validation dependences

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import os
 
+import pytest
 import testinfra.utils.ansible_runner
+from hamcrest import assert_that, equal_to
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE'],
@@ -9,8 +11,20 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+    exe = host.file('/etc/hosts')
+    assert_that(exe.exists, equal_to(True))
+    assert_that(exe.user, equal_to('root'))
+    assert_that(exe.group, equal_to('root'))
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+@pytest.mark.parametrize(
+    'name', [
+        ('vim'),
+        ('curl'),
+        ('git'),
+        ('gcc'),
+    ],
+)
+def test_ruby_build_dependencies(host, name):
+    pkg = host.package(name)
+    assert_that(pkg.is_installed, equal_to(True))
